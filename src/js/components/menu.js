c878e3cfde7b4041c9b1cfcf5da6b2e83435f35d
ustsepavy <vladstepovoyvios@gmail.com>
2020-11-27 17:18:11 +0300

    feat: adding categories

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,3 +1,6 @@
+import create from '../utils/create';
+import CardsContainer from './cards-container';
+
 export default class Menu {
   constructor(settings) {
     this.settings = settings;
@@ -6,5 +9,31 @@ export default class Menu {
 
   init = () => {
     this.loader.show();
+    const mainContainer = document.querySelector('.main__container');
+    this.cardsContainer = create('div', 'cards__container', '', mainContainer);
+    this.categories = [];
+    const cards = new CardsContainer();
+    cards.load().then((res) => {
+      const cardsCategories = res[0];
+      console.log(cardsCategories);
+      for (let i = 0; i < cardsCategories.length; i++) {
+        this.categories.push(
+          create(
+            'div',
+            'card__category',
+            create('div', '', [
+              create('div', 'name', cardsCategories[i].name),
+              create('img', 'image', '', '', [
+                'src',
+                cardsCategories[i].imgUrl,
+              ]),
+            ]),
+            this.cardsContainer,
+            ['id', cardsCategories[i].id]
+          )
+        );
+      }
+      console.log(this.cardsContainer);
+    });
   };
 }
